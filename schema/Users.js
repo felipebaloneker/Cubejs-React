c99ca312641852333770b31c1ea23ce83758a987
felipe baloneker <felipebaloneker23@gmail.com>
2022-05-19 16:38:36 -0300

    first commit

---
+++ schema/Users.js
@@ -0,0 +1,39 @@
+cube(`Users`, {
+  sql: `SELECT * FROM public.users`,
+  
+  preAggregations: {
+    // Pre-Aggregations definitions go here
+    // Learn more here: https://cube.dev/docs/caching/pre-aggregations/getting-started  
+  },
+  
+  joins: {
+    
+  },
+  
+  measures: {
+    count: {
+      type: `count`,
+      drillMembers: [id, name]
+    }
+  },
+  
+  dimensions: {
+    password: {
+      sql: `password`,
+      type: `string`
+    },
+    
+    id: {
+      sql: `id`,
+      type: `string`,
+      primaryKey: true
+    },
+    
+    name: {
+      sql: `name`,
+      type: `string`
+    }
+  },
+  
+  dataSource: `default`
+});

44f3444008d74b769fd02e9eb6d3796c18c99642
felipe baloneker <felipebaloneker23@gmail.com>
2022-05-19 17:35:26 -0300

    redenring linechart

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { LineChart, Line, XAxis, YAxis } from 'recharts';
 import cubejs from '@cubejs-client/core';
 import { QueryRenderer } from '@cubejs-client/react';
@@ -8,26 +8,36 @@ const cubejsApi = cubejs(process.env.REACT_APP_CUBEJS_API_SECRET, {
 });
 
 export default () => {
-  console.log(cubejsApi)
+  const [data,setData] = useState([])
   return (
     <QueryRenderer
       query={{
-        measures: ['Stories.count'],
-        dimensions: ['Stories.time.month'],
+        measures: ['Orders.count'],
+        dimensions: ['Orders.status'],
       }}
       cubejsApi={cubejsApi}
       render={({ resultSet }) => {
         if (!resultSet) {
           return 'Loading...';
         }
+        return(
+          <LineChart 
+            width={600} 
+            height={400}       
+            margin={{
+            top: 10,
 
-        return (
-          <LineChart data={resultSet.rawData()}>
-            <XAxis dataKey="Stories.time" />
-            <YAxis />
-            <Line type="monotone" dataKey="Stories.count" stroke="#8884d8" />
+          }} data={resultSet.rawData()}>
+          <XAxis dataKey="Orders.status" />
+          <YAxis />
+          <Line
+            type="monotone"
+            dataKey="Orders.count"
+            stroke="#8884d8"
+          />
+          <Line type="monotone" dataKey="Orders.status" stroke="#82ca9d" />
           </LineChart>
-        );
+        )
       }}
     />
   );
--- schema/Orders.js
+++ schema/Orders.js
@@ -1,36 +1,28 @@
 cube(`Orders`, {
-  sql: `
-  select 1 as id, 100 as amount, 'new' status
-  UNION ALL
-  select 2 as id, 200 as amount, 'new' status
-  UNION ALL
-  select 3 as id, 300 as amount, 'processed' status
-  UNION ALL
-  select 4 as id, 500 as amount, 'processed' status
-  UNION ALL
-  select 5 as id, 600 as amount, 'shipped' status
-  `,
-
+  sql: `SELECT * FROM public.orders`,
+  
   preAggregations: {
     // Pre-Aggregations definitions go here
-    // Learn more here: https://cube.dev/docs/caching/pre-aggregations/getting-started
+    // Learn more here: https://cube.dev/docs/caching/pre-aggregations/getting-started  
   },
-
+  
+  joins: {
+    
+  },
+  
   measures: {
     count: {
-      type: `count`
-    },
-
-    totalAmount: {
-      sql: `amount`,
-      type: `sum`
+      type: `count`,
+      drillMembers: []
     }
   },
-
+  
   dimensions: {
     status: {
       sql: `status`,
       type: `string`
     }
-  }
+  },
+  
+  dataSource: `default`
 });
